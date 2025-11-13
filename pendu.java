fd2fe9ca7827210f864238892368250fa17f8bb3
Jeremybyc <jbc60510@outlook.fr>
2025-11-13 10:09:01 +0100

    unsufull function convert deleted and code workable

--- pendu.java
+++ pendu.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 class pendu {
-    public static String[] possibleWords = { "Java", "Python", "Javascript", "Gnome" };
+    public static String[] possibleWords = { "java", "python", "javascript", "gnome" };
 
     public static Scanner scan = new Scanner(System.in);
 
@@ -16,16 +16,17 @@ class pendu {
         String wordToFind = possibleWords[new Random().nextInt(possibleWords.length - 0) + 0];
         char[] letters = wordToFind.toCharArray();
         char[] hiddenWord = hideWord(letters);
+
         int numberOfTry = 0;
         boolean fullyUncovered = wordFund(hiddenWord);
         while (numberOfTry < 10) {
             display(hiddenWord);
+            propose(hiddenWord, letters, numberOfTry);
             numberOfTry = propose(hiddenWord, letters, numberOfTry);
-            wordFund(hiddenWord);
-
+            fullyUncovered = wordFund(hiddenWord);
             if (fullyUncovered) {
-                System.out.println("Vous avez gagné en " + numberOfTry);
-
+                System.out.println("Vous avez gagné en " + numberOfTry + " coups");
+                rematch(numberOfTry, fullyUncovered);
             }
         }
         rematch(numberOfTry, fullyUncovered);
@@ -34,7 +35,7 @@ class pendu {
 
     // convertit le mot en différents '_'
     public static char[] hideWord(char[] letters) {
-        char[] hiddenletters = letters;
+        char[] hiddenletters = letters.clone();
         for (int i = 0; i < hiddenletters.length; i++) {
             hiddenletters[i] = '_';
         }
@@ -43,8 +44,7 @@ class pendu {
 
     public static int propose(char[] hiddenWord, char[] letters, int numberOfTry) {
         char letterProposed = Character.toLowerCase(scan.next().charAt(0));
-        // convertit la lettre proposée du scanner en valeur de code
-        letterProposed = convert(letterProposed);
+
         boolean isLetterFound = false;
         // regarde si c'est une lettre de l'alphabet
         for (char letterToFind : alphabet) {
@@ -52,6 +52,7 @@ class pendu {
             if (letterToFind == letterProposed) {
                 isLetterFound = true;
                 break;
+
             }
         }
         // si la lettre n'est trouvée
@@ -63,11 +64,14 @@ class pendu {
             // si la lettre est trouvée, on regarde dans le mot originel non codé si la
             // lettre est présente, et si c'est le cas, la lettre vient désormais apparaitre
             // dans le mot codé à la place d'un '_'
+
             for (char letter : letters) {
+
                 if (letterProposed == letter) {
                     hiddenWord[i] = letterProposed;
                     finded++;
                 }
+                i++;
             }
             if (finded == 0) {
                 System.out.println("Il n'y a pas la lettre " + letterProposed + " dans le mot que vous recherchez");
@@ -90,98 +94,6 @@ class pendu {
         }
         fullyUncovered = true;
         return fullyUncovered;
-    }
-
-    public static char convert(char letterProposed) {
-        switch (letterProposed) {
-            case 'a':
-                letterProposed = 'a';
-                break;
-            case 'b':
-                letterProposed = 'b';
-                break;
-            case 'c':
-                letterProposed = 'c';
-                break;
-            case 'd':
-                letterProposed = 'd';
-                break;
-            case 'e':
-                letterProposed = 'e';
-                break;
-            case 'f':
-                letterProposed = 'f';
-                break;
-            case 'g':
-                letterProposed = 'g';
-                break;
-            case 'h':
-                letterProposed = 'h';
-                break;
-
-            case 'i':
-                letterProposed = 'i';
-                break;
-            case 'j':
-                letterProposed = 'j';
-                break;
-            case 'k':
-                letterProposed = 'k';
-                break;
-            case 'l':
-                letterProposed = 'l';
-                break;
-            case 'm':
-                letterProposed = 'm';
-                break;
-            case 'n':
-                letterProposed = 'n';
-                break;
-            case 'o':
-                letterProposed = 'o';
-                break;
-
-            case 'p':
-                letterProposed = 'p';
-                break;
-            case 'q':
-                letterProposed = 'q';
-
-                break;
-            case 'r':
-                letterProposed = 'r';
-                break;
-            case 's':
-                letterProposed = 's';
-
-                break;
-            case 't':
-                letterProposed = 't';
-                break;
-            case 'u':
-                letterProposed = 'u';
-                break;
-            case 'v':
-                letterProposed = 'v';
-                break;
-
-            case 'w':
-                letterProposed = 'w';
-                break;
-            case 'x':
-                letterProposed = 'x';
-                break;
-            case 'y':
-                letterProposed = 'y';
-                break;
-            case 'z':
-                letterProposed = 'z';
-                break;
-            default:
-                break;
-
-        }
-        return letterProposed;
     }
 
     // propose un rematch
